06e9ad416f990d31e9996175cbe6f380e49b5cfb
dekstr0metorfan <jotszotyt@gmail.com>
2024-07-18 02:49:18 +0200

    Foreign currency -> PLN conversion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         NBP.NBP_converter();
     }
 }
--- src/main/java/org/example/NBP.java
+++ src/main/java/org/example/NBP.java
@@ -9,10 +9,18 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.*;
+
 public class NBP
 {
-    public static void NBP_converter()
-    {
+    public static void NBP_converter() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter amount of currency you want to get: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter code of currency (according to ISO 4217): ");
+        String currency = scanner.nextLine();
+
         String url = "https://api.nbp.pl/api/exchangerates/tables/a/";
         try (CloseableHttpClient httpClient = HttpClients.createDefault())
         {
@@ -29,25 +37,33 @@ public class NBP
                     JSONObject jsonObject = jsonArray.getJSONObject(0);
                     JSONArray rates = jsonObject.getJSONArray("rates");
 
-                    StringBuilder result = new StringBuilder();
+                    HashMap<String, Double> result = new HashMap<>();
                     for (int i = 0; i < rates.length(); i++) {
                         JSONObject rate = rates.getJSONObject(i);
                         String code = rate.getString("code");
                         double mid = rate.getDouble("mid");
-                        result.append(code).append(" : ").append(mid).append("\n");
+                        result.put(code, mid);
                     }
 
-                    System.out.println(result);
+                    double outcome = 0;
+                    if(result.containsKey(currency))
+                    {
+                        outcome = amount * result.get(currency);
+
+                        System.out.print("Price of this currency is ");
+                        System.out.printf("%.2f", outcome);
+                        System.out.println(" PLN");
+                    }
+                    else
+                    {
+                        System.out.println("Wrong currency code!");
+                    }
                 }
                 else
                 {
                     System.err.println("Error code: " + statusCode);
                 }
             }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
         }
     }
 }
